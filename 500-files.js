1e54287852c9937da4ce7a74b8e6dc95043cbe35
Ian Hattendorf <ianh@axosoft.com>
2019-08-13 10:45:07 -0700

    Cause conflict

--- 500-files.js
+++ 500-files.js
@@ -2,8 +2,18 @@ var fs = require('fs');
 
 try {
   fs.mkdir('./500_files');
+
+  console.log('merge conflicts');
+
+
+  console.log('are annoying');
 } catch(e) {
   // do nothing
+
+
+
+
+  console.log('except log');
 }
 
 for (var i = 100; i < 600; ++i) {
